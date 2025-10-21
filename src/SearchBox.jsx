9fbbee73ce9356ed13d25901830c6039ad135748
alok raj <alokraj1319@gmail.com>
2025-10-21 17:14:54 +0530

    getWeatherInfo function that brings data from the api using url and key

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -6,6 +6,16 @@ import { useState } from 'react';
 export default function SearchBox() {
     let [city, setCity] = useState("");
 
+    const API_URL = "http://api.openweathermap.org/geo/1.0/direct";
+    const API_KEY = "5c9b257ede27e4138f7654f28ec4ca19";
+
+    let getWeatherInfo = async () => {
+        let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}`);
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+    };
+
+
     let handleChange = (evt) => {
         setCity(evt.target.value);
     };
