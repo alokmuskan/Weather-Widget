d31523dfb082c84380968823cb477f02e14a3363
alok raj <alokraj1319@gmail.com>
2025-10-21 16:21:47 +0530

    created a input field and a search button

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -1,7 +1,14 @@
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
 export default function SearchBox() {
     return (
         <div>
             <h3>Search for the Weather</h3>
+            <form>
+                <TextField id="city" label="City Name" variant="outlined" required/>
+                <Button variant="contained">Search</Button>
+            </form>
         </div>
     );
 }
