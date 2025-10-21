2d34bb231001291659cf259b10688595bd88aa6e
alok raj <alokraj1319@gmail.com>
2025-10-21 16:24:57 +0530

    shifted the button in next line

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -7,6 +7,7 @@ export default function SearchBox() {
             <h3>Search for the Weather</h3>
             <form>
                 <TextField id="city" label="City Name" variant="outlined" required/>
+                <br /><br />
                 <Button variant="contained" type='submit'>Search</Button>
             </form>
         </div>
